5581aba6e7cca8d6ca276b57aa34cdb4037404d2
fyusuf-a <fyusuf-a@student.42.fr>
2021-02-23 12:37:54 +0100

    Mysql entrypoint fixed to allow multiple runs on the same volume

--- mysql/srcs/docker-entrypoint.sh
+++ mysql/srcs/docker-entrypoint.sh
@@ -1,2 +1,4 @@
-mysql_install_db --user=mysql > /dev/null
+if [ -z "$(ls -A /data)" ]; then
+	mysql_install_db --user=mysql > /dev/null
+fi
 mysqld --user=mysql --datadir=/data --init-file=/create.sql
