bb866da458df62e08ebeb0a257acc7ab07516136
fyusuf-a <fyusuf-a@student.42.fr>
2021-03-18 15:25:39 +0100

    Grafana, prometheus and influxdb are three different containers working with volumes in docker-compose

--- grafana/srcs/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-[ ! -d /usr/share/grafana/conf ] && cp -R /grafana-files/conf /usr/share/grafana/conf
-[ ! -d /usr/share/grafana/public ] && cp -R /grafana/public /usr/share/grafana/public
-grafana-server > /var/log/grafana.log &
-tail -f /var/log/grafana.log
