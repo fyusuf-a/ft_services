dc1549f5a1c0ce1e1abe0e3b2eb5a5f99b6c7401
fyusuf-a <fyusuf-a@student.42.fr>
2021-03-25 13:48:37 +0100

    Wordpress, mysql, grafana, influxdb and prometheus and ftps working correctly with healthchecks

--- grafana/srcs/docker-entrypoint.sh
+++ grafana/srcs/docker-entrypoint.sh
@@ -5,7 +5,7 @@ if [ ! -f /usr/share/grafana/data/grafana.db ]; then
 	mkdir -p /usr/share/grafana/data
 	cp /grafana.db /usr/share/grafana/data/grafana.db
 fi
-rm /grafana.db
+rm -f /grafana.db
 
 grafana-server > /var/log/grafana.log &
 tail -f /var/log/grafana.log
--- wordpress/srcs/docker-entrypoint.sh
+++ wordpress/srcs/docker-entrypoint.sh
@@ -1,2 +1,15 @@
 php-fpm7 -D
+
+CONTINUE_SCRIPT=1
+while [ $CONTINUE_SCRIPT = 1 ]; do
+	nc -z -w 2 $WORDPRESS_DB_HOST 3306
+	if [ $? = 0 ]; then
+		wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST 2>/dev/null
+		wp core install --url=$URL --title=$TITLE --admin_user=$ADMIN_USER --admin_email=$ADMIN_EMAIL --admin_password=$ADMIN_PASSWORD --skip-email
+		wp user create $EDITOR_USER $EDITOR_EMAIL --user_pass=$EDITOR_PASSWORD 2>/dev/null
+		wp user create $AUTHOR_USER $AUTHOR_EMAIL --user_pass=$AUTHOR_PASSWORD 2>/dev/null
+		CONTINUE_SCRIPT=0
+	fi
+done
+
 nginx -g 'daemon off;'
