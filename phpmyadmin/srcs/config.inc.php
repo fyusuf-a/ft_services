87164dc54294918783d8a620710117482dc31840
fyusuf-a <fyusuf-a@student.42.fr>
2021-03-26 09:43:15 +0100

    All containers (except metalLB and nginx) are healthy

--- phpmyadmin/srcs/config.inc.php
+++ phpmyadmin/srcs/config.inc.php
@@ -29,9 +29,9 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = 'mysql:3306';
-$cfg['Servers'][$i]['user'] = 'wordpress';
-$cfg['Servers'][$i]['password'] = 'password';
+$cfg['Servers'][$i]['host'] = '%WORDPRESS_DB_HOST%';
+$cfg['Servers'][$i]['user'] = '%WORDPRESS_DB_USER%';
+$cfg['Servers'][$i]['password'] = '%WORDPRESS_DB_PASSWORD%';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
 
--- phpmyadmin/srcs/docker-entrypoint.sh
+++ phpmyadmin/srcs/docker-entrypoint.sh
@@ -4,6 +4,9 @@ CONTINUE_SCRIPT=1
 while [ $CONTINUE_SCRIPT = 1 ]; do
 	nc -z -w 2 $WORDPRESS_DB_HOST
 	if [ $? = 0 ]; then
+		sed -i 's/%WORDPRESS_DB_HOST%/'$WORDPRESS_DB_HOST'/g' /etc/phpmyadmin/config.inc.php
+		sed -i 's/%WORDPRESS_DB_USER%/'$WORDPRESS_DB_USER'/g' /etc/phpmyadmin/config.inc.php
+		sed -i 's/%WORDPRESS_DB_PASSWORD%/'$WORDPRESS_DB_PASSWORD'/g' /etc/phpmyadmin/config.inc.php
 		CONTINUE_SCRIPT=0
 	fi
 done
