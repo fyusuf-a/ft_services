951f16faa91107bc88844e7ab22e5e5167eead9c
fyusuf-a <fyusuf-a@student.42.fr>
2021-04-10 19:39:25 +0200

    Moved files in a srcs subdirectory

--- start.sh
+++ start.sh
@@ -2,19 +2,19 @@ minikube delete
 minikube start --vm-driver=docker
 IP=$(minikube ip)
 
-cat metallb.yaml | sed "s/%IP%/$IP/g" | kubectl apply -f -
+cat srcs/metallb.yaml | sed "s/%IP%/$IP/g" | kubectl apply -f -
 # On first install only
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 
 eval $(minikube -p minikube docker-env)
-docker build ftps -t ftps --build-arg IP=$IP
-docker build wordpress -t wordpress
-docker build mysql -t mysql
-docker build phpmyadmin -t phpmyadmin
-docker build nginx -t nginx --build-arg WORDPRESS_EXTERNAL_NAME=$IP --build-arg PHPMYADMIN_DNS_NAME='phpmyadmin'
+docker build srcs/ftps -t ftps --build-arg IP=$IP
+docker build srcs/wordpress -t wordpress
+docker build srcs/mysql -t mysql
+docker build srcs/phpmyadmin -t phpmyadmin
+docker build srcs/nginx -t nginx --build-arg WORDPRESS_EXTERNAL_NAME=$IP --build-arg PHPMYADMIN_DNS_NAME='phpmyadmin'
 
-kubectl apply -f ftps.yaml
-cat wordpress.yaml | sed "s/%IP%/$IP/g" | kubectl apply -f -
-kubectl apply -f mysql.yaml
-kubectl apply -f phpmyadmin.yaml
-kubectl apply -f nginx.yaml
+kubectl apply -f srcs/ftps.yaml
+cat srcs/wordpress.yaml | sed "s/%IP%/$IP/g" | kubectl apply -f -
+kubectl apply -f srcs/mysql.yaml
+kubectl apply -f srcs/phpmyadmin.yaml
+kubectl apply -f srcs/nginx.yaml
