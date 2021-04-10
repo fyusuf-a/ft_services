db5a7cf2f229fe1c90f9429ca01360798c91a3fd
fyusuf-a <fyusuf-a@student.42.fr>
2021-04-10 20:41:20 +0200

    All pods are seemingly working

--- start.sh
+++ start.sh
@@ -7,14 +7,18 @@ cat srcs/metallb.yaml | sed "s/%IP%/$IP/g" | kubectl apply -f -
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 
 eval $(minikube -p minikube docker-env)
+docker build srcs/mysql -t mysql
+docker build srcs/influxdb -t influxdb
 docker build srcs/ftps -t ftps --build-arg IP=$IP
 docker build srcs/wordpress -t wordpress
-docker build srcs/mysql -t mysql
 docker build srcs/phpmyadmin -t phpmyadmin
 docker build srcs/nginx -t nginx --build-arg WORDPRESS_EXTERNAL_NAME=$IP --build-arg PHPMYADMIN_DNS_NAME='phpmyadmin'
+docker build srcs/grafana -t grafana
 
+kubectl apply -f srcs/mysql.yaml
+kubectl apply -f srcs/influxdb.yaml
 kubectl apply -f srcs/ftps.yaml
 cat srcs/wordpress.yaml | sed "s/%IP%/$IP/g" | kubectl apply -f -
-kubectl apply -f srcs/mysql.yaml
 kubectl apply -f srcs/phpmyadmin.yaml
 kubectl apply -f srcs/nginx.yaml
+kubectl apply -f srcs/grafana.yaml
