29fbb959cdf1be73c1c0c6906d9f71697701e865
fyusuf-a <fyusuf-a@student.42.fr>
2021-04-10 19:35:28 +0200

    ftps, mysql, wordpress, nginx, phpmyadmin pods are working as far as networking is concerned

--- start.sh
+++ start.sh
@@ -10,7 +10,11 @@ eval $(minikube -p minikube docker-env)
 docker build ftps -t ftps --build-arg IP=$IP
 docker build wordpress -t wordpress
 docker build mysql -t mysql
+docker build phpmyadmin -t phpmyadmin
+docker build nginx -t nginx --build-arg WORDPRESS_EXTERNAL_NAME=$IP --build-arg PHPMYADMIN_DNS_NAME='phpmyadmin'
 
 kubectl apply -f ftps.yaml
 cat wordpress.yaml | sed "s/%IP%/$IP/g" | kubectl apply -f -
 kubectl apply -f mysql.yaml
+kubectl apply -f phpmyadmin.yaml
+kubectl apply -f nginx.yaml
