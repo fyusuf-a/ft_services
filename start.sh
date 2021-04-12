a0c2b515e8e579870899f554663948f416a22c51
fyusuf-a <fyusuf-a@student.42.fr>
2021-04-12 11:27:46 +0200

    Added networking in all pods, problem with minikube-dashboard and nginx pod

--- start.sh
+++ start.sh
@@ -1,24 +1,34 @@
 minikube delete
-minikube start --vm-driver=docker
+minikube start --cpus 2 --vm-driver=docker
 IP=$(minikube ip)
+minikube addons enable metrics-server
 
+# Installation of metallb
 cat srcs/metallb.yaml | sed "s/%IP%/$IP/g" | kubectl apply -f -
 # On first install only
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 
+# Installation of prometheus
+kubectl apply -f srcs/prometheus.yaml
+
 eval $(minikube -p minikube docker-env)
 docker build srcs/mysql -t mysql
-docker build srcs/influxdb -t influxdb
-docker build srcs/ftps -t ftps --build-arg IP=$IP
-docker build srcs/wordpress -t wordpress
-docker build srcs/phpmyadmin -t phpmyadmin
-docker build srcs/nginx -t nginx --build-arg WORDPRESS_EXTERNAL_NAME=$IP --build-arg PHPMYADMIN_DNS_NAME='phpmyadmin'
-docker build srcs/grafana -t grafana
-
 kubectl apply -f srcs/mysql.yaml
+
+docker build srcs/influxdb -t influxdb
 kubectl apply -f srcs/influxdb.yaml
+
+docker build srcs/ftps -t ftps --build-arg IP=$IP
 kubectl apply -f srcs/ftps.yaml
+
+docker build srcs/wordpress -t wordpress
 cat srcs/wordpress.yaml | sed "s/%IP%/$IP/g" | kubectl apply -f -
+
+docker build srcs/phpmyadmin -t phpmyadmin
 kubectl apply -f srcs/phpmyadmin.yaml
+
+docker build srcs/nginx -t nginx --build-arg WORDPRESS_EXTERNAL_NAME=$IP --build-arg PHPMYADMIN_DNS_NAME='phpmyadmin'
 kubectl apply -f srcs/nginx.yaml
+
+docker build srcs/grafana -t grafana
 kubectl apply -f srcs/grafana.yaml
